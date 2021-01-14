2e39eaee0a248ffe7e64904e0c1a975e274c9aa4
formaggia <luca.formaggia@gmail.com>
2021-01-14 17:34:04 +0100

    updated

--- install_PACS.sh
+++ install_PACS.sh
@@ -10,7 +10,7 @@ cd build/
 # run cmake
 #
 # this creates the dynamic libraries and optimised code
-cmake -D CMAKE_INSTALL_PREFIX=../../../Examples/ ../
+cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=../../../Examples/ ../
 
 # uncomment this line (and comment the previous one)
 #if you want to debug, this also creates the static libraries
@@ -18,10 +18,13 @@ cmake -D CMAKE_INSTALL_PREFIX=../../../Examples/ ../
 #cmake -D CMAKE_BUILD_TYPE=Debug -D CMAKE_INSTALL_PREFIX=../../../Examples/ ../
 
 #compile
-make
+make -j 2
+# Remove ald vesrions of the library (with -f rm does not complain if the file does not exist)
+rm -f ../../../Examples/lib/libmuparserx.a
+rm -f ../../../Examples/lib/libmuparserx.so
 #install
 make install
 
-# You can remuve the files in the build/ directory now if you want
+# You can remove the files in the build/ directory now
 cd ..
 rm -r -f build/
